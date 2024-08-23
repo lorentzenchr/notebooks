a698f9e62c4c0c61538a7204bfc0d2501da6fa5a
Michael Mayer <mayermichael79@gmail.com>
2024-08-23 11:40:43 +0200

    Add files via upload

---
+++ blogposts/2024-08-23-missranger.R
@@ -0,0 +1,43 @@
+library(missRanger)
+
+# 10% missings
+ir <- iris |> 
+  generateNA(p = 0.1, seed = 11)
+
+# Train/test split stratified by Species
+oos <- c(1:10, 51:60, 101:110)
+train <- ir[-oos, ]
+test <- ir[oos, ]
+
+head(test)
+
+#   Sepal.Length Sepal.Width Petal.Length Petal.Width Species
+# 1          5.1         3.5          1.4         0.2  setosa
+# 2          4.9         3.0          1.4         0.2  setosa
+# 3          4.7         3.2          1.3          NA  setosa
+# 4          4.6         3.1          1.5         0.2  setosa
+# 5          5.0         3.6          1.4         0.2  setosa
+# 6          5.4          NA          1.7          NA  setosa
+
+mr <- missRanger(train, pmm.k = 5, keep_forests = TRUE, seed = 1)
+test_filled <- predict(mr, test, seed = 1)
+head(test_filled)
+
+#   Sepal.Length Sepal.Width Petal.Length Petal.Width Species
+# 1          5.1         3.5          1.4         0.2  setosa
+# 2          4.9         3.0          1.4         0.2  setosa
+# 3          4.7         3.2          1.3         0.2  setosa
+# 4          4.6         3.1          1.5         0.2  setosa
+# 5          5.0         3.6          1.4         0.2  setosa
+# 6          5.4         4.0          1.7         0.4  setosa
+
+# Original
+head(iris)
+
+#   Sepal.Length Sepal.Width Petal.Length Petal.Width Species
+# 1          5.1         3.5          1.4         0.2  setosa
+# 2          4.9         3.0          1.4         0.2  setosa
+# 3          4.7         3.2          1.3         0.2  setosa
+# 4          4.6         3.1          1.5         0.2  setosa
+# 5          5.0         3.6          1.4         0.2  setosa
+# 6          5.4         3.9          1.7         0.4  setosa
